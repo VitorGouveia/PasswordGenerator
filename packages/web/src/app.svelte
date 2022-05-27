e3e3e2597f63f0c0aada408d049c176a072c3754
Vitor Gouveia <vitorneves.gouveia10@gmail.com>
2022-05-26 21:20:55 -0300

    chore: add storybook

--- packages/web/src/app.svelte
+++ packages/web/src/app.svelte
@@ -1,20 +1,36 @@
 <script lang="ts">
-  import Router, { link } from "svelte-spa-router";
-  import { wrap } from "svelte-spa-router/wrap";
+  // import Router, { link } from "svelte-spa-router";
+  // import { wrap } from "svelte-spa-router/wrap";
+
+  import { Router } from "svelte-router-spa";
 
   import "./styles/global.scss";
 
+  import Home from "./pages/home.svelte";
   import StyleGuide from "./pages/style-guide.svelte";
   import NotFound from "./pages/not-found.svelte";
+  const routes = [
+    {
+      name: "/",
+      component: Home,
+    },
+    {
+      name: "/style-guide",
+      component: StyleGuide,
+    },
+  ];
 
-  const routes = {
-    "/": wrap({
-      asyncComponent: () => import("./pages/home.svelte"),
-    }),
-    "/style-guide": StyleGuide,
-    "*": NotFound,
-  };
+  // const routes = {
+  //   "/": wrap({
+  //     asyncComponent: () => import("./pages/home.svelte"),
+  //   }),
+  //   "/storybook": wrap({}),
+  //   "/style-guide": StyleGuide,
+  //   "*": NotFound,
+  // };
 </script>
 
-<Router restoreScrollState={true} {routes} />
-<!-- <a href="/style-guide" use:link>bruh</a> -->
+<!-- <Router {routes} prefix="PasswordGenerator" /> -->
+<Router {routes} />
+<!-- <Link to="/style-guide">style guide</Link> -->
+<a href="/style-guide">bruh</a>
