b2899085b4bcd0739078fc87217ca9c95dcba2fd
Vitor Gouveia <vitorneves.gouveia10@gmail.com>
2022-05-26 21:27:45 -0300

    chore(web): test hash router

--- packages/web/src/app.svelte
+++ packages/web/src/app.svelte
@@ -1,36 +1,23 @@
 <script lang="ts">
-  // import Router, { link } from "svelte-spa-router";
-  // import { wrap } from "svelte-spa-router/wrap";
-
-  import { Router } from "svelte-router-spa";
+  import Router, { link } from "svelte-spa-router";
+  import { wrap } from "svelte-spa-router/wrap";
 
   import "./styles/global.scss";
 
   import Home from "./pages/home.svelte";
   import StyleGuide from "./pages/style-guide.svelte";
   import NotFound from "./pages/not-found.svelte";
-  const routes = [
-    {
-      name: "/",
-      component: Home,
-    },
-    {
-      name: "/style-guide",
-      component: StyleGuide,
-    },
-  ];
 
-  // const routes = {
-  //   "/": wrap({
-  //     asyncComponent: () => import("./pages/home.svelte"),
-  //   }),
-  //   "/storybook": wrap({}),
-  //   "/style-guide": StyleGuide,
-  //   "*": NotFound,
-  // };
+  const routes = {
+    "/": wrap({
+      asyncComponent: () => import("./pages/home.svelte"),
+    }),
+    "/style-guide": StyleGuide,
+    "*": NotFound,
+  };
 </script>
 
 <!-- <Router {routes} prefix="PasswordGenerator" /> -->
 <Router {routes} />
 <!-- <Link to="/style-guide">style guide</Link> -->
-<a href="/style-guide">bruh</a>
+<a use:link href="/style-guide">bruh</a>
