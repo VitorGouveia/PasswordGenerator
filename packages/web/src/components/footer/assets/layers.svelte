84e1187d50f7dd3c2082f05b78ff100993f137fc
Vitor Gouveia <vitorneves.gouveia10@gmail.com>
2022-05-28 00:33:55 -0300

    fix(web): add figma, style-guide and storybook links

---
+++ packages/web/src/components/footer/assets/layers.svelte
@@ -0,0 +1,5 @@
+<svg width="10" height="10" viewBox="0 0 10 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M5.00004 0.833374L0.833374 2.91671L5.00004 5.00004L9.16671 2.91671L5.00004 0.833374Z" stroke="#C5C2CB" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M0.833374 7.08337L5.00004 9.16671L9.16671 7.08337" stroke="#C5C2CB" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M0.833374 5L5.00004 7.08333L9.16671 5" stroke="#C5C2CB" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
--- packages/web/src/components/footer/index.svelte
+++ packages/web/src/components/footer/index.svelte
@@ -1,16 +1,27 @@
 <script lang="ts">
-  import Button from "../button/index.svelte";
   import Link from "../link.svelte";
+  import Button from "../button/index.svelte";
 
+  import Figma from "./assets/figma.svelte";
   import Flag from "./assets/flag.svelte";
   import Github from "./assets/github.svelte";
+  import Layers from "./assets/layers.svelte";
   import Moon from "./assets/moon.svelte";
+  import Package from "./assets/package.svelte";
 
   function handleThemeChange() {}
 </script>
 
 <footer>
-  <Button classname="svg-button" variant="outlined">
+  <a
+    target="_blank"
+    href="https://www.figma.com/file/JOp7LjI8QWZPL0GTLE4BPX/PasswordGenerator?node-id=8%3A10"
+  >
+    <Button classname="svg-button" variant="outlined">
+      <Figma />
+    </Button>
+  </a>
+  <Button disabled classname="svg-button" variant="outlined">
     <Flag />
   </Button>
   <a target="_blank" href="https://github.com/vitorGouveia/PasswordGenerator">
@@ -18,7 +29,21 @@
       <Github />
     </Button>
   </a>
+  <a
+    target="_blank"
+    href="https://vitorgouveia.github.io/PasswordGenerator/storybook"
+  >
+    <Button classname="svg-button" variant="outlined">
+      <Package />
+    </Button>
+  </a>
+  <Link href="/style-guide">
+    <Button classname="svg-button" variant="outlined">
+      <Layers />
+    </Button>
+  </Link>
   <Button
+    disabled
     classname="svg-button"
     variant="outlined"
     on:click={handleThemeChange}
